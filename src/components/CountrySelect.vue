4dfe98911c923afb520c0e2f342bec96a32ee8b6
Liam Mandeville <liamman1990@gmail.com>
2022-08-03 15:20:35 -0700

    Finished app

--- src/components/CountrySelect.vue
+++ src/components/CountrySelect.vue
@@ -2,7 +2,7 @@
   <select @change="onChange()"
   v-model="selected" class="form-select mt-10 block w-full border p-3 rounded">
     <option value="0">Select Country</option>
-    <option v-for="country in countries" value="country.ID">
+    <option v-for="country in countries" :value="country.ID">
       {{ country.Country }}
     </option>
   </select>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,12 @@
 
   <CountrySelect @get-country="getCountryData" :countries="countries" />
 
+  <button @click="clearCountryData"
+    v-if="stats.Country"
+    class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600">
+    Clear Country
+  </button>
+
   </main>
 
   <main class="flex flex-col align-center justify-center text-center" v-else>
@@ -47,6 +53,13 @@ export default {
     getCountryData(country) {
       this.stats = country
       this.title = country.Country
+    },
+    async clearCountryData() {
+      this.loading = true
+      const data = await this.fetchCovidData()
+      this.title = 'Global'
+      this.stats = data.Global
+      this.loading = false
     }
   },
   async created() {
